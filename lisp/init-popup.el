78d2ea74b0375e633c28dc4050aa8fca3e6410b1
b40yd <bb.qnyd@gmail.com>
2024-11-04 18:27:04 +0800

    Add docker popup.

--- lisp/init-popup.el
+++ lisp/init-popup.el
@@ -973,38 +973,39 @@ prevent the popup(s) from messing up the UI (or vice versa)."
 ;; Default popup rules & bootstrap
 
 (set-popup-rules!
- '(("^\\*Completions" :ignore t)
-   ("^\\*Local variables\\*$"
-    :vslot -1 :slot 1 :size +popup-shrink-to-fit)
-   ("^\\*\\(?:[Cc]ompil\\(?:ation\\|e-Log\\)\\|Messages\\)"
-    :vslot -2 :size 0.3  :autosave t :quit t :ttl nil)
-   ("^\\*\\(?:doom \\|Pp E\\)"  ; transient buffers (no interaction required)
-    :vslot -3 :size +popup-shrink-to-fit :autosave t :select ignore :quit t :ttl 0)
-   ("^\\*\\(?:v?term\\|e?shell\\)"  ; editing buffers (interaction required)
-    :vslot -5 :size 0.25 :select t :modeline nil :quit nil :ttl nil)
-   ("^\\*\\(?:Wo\\)?Man "
-    :vslot -6 :size 0.45 :select t :quit t :ttl 0)
-   ("^\\*Calc"
-    :vslot -7 :side bottom :size 0.4 :select t :quit nil :ttl 0)
-   ("^\\*Customize"
-    :slot 2 :side right :size 0.5 :select t :quit nil)
-   ("^ \\*undo-tree\\*"
-    :slot 2 :side left :size 20 :select t :quit t)
-   ;; `help-mode', `helpful-mode'
-   ("^\\*\\([Hh]elp\\|Apropos\\)"
-    :slot 2 :vslot -8 :size 0.42 :select t)
-   ("^\\*eww\\*"  ; `eww' (and used by dash docsets)
-    :vslot -11 :size 0.35 :select t)
-   ("^\\*xwidget"
-    :vslot -11 :size 0.35 :select nil)
-   ("^\\*info\\*$"  ; `Info-mode'
-    :slot 2 :vslot 2 :size 0.45 :select t)
-   ("^\\*Warnings" :vslot 99 :size 0.25)
-   ("^\\*Backtrace" :vslot 99 :size 0.4 :quit nil)
-   ("^\\*CPU-Profiler-Report "    :side bottom :vslot 100 :slot 1 :height 0.4 :width 0.5 :quit nil)
-   ("^\\*Memory-Profiler-Report " :side bottom :vslot 100 :slot 2 :height 0.4 :width 0.5 :quit nil)
-   ("^\\*Process List\\*" :side bottom :vslot 101 :size 0.25 :select t :quit t)
-   ("^\\*\\(?:Proced\\|timer-list\\|Abbrevs\\|Output\\|Occur\\|unsent mail.*?\\|message\\)\\*" :ignore t)))
+  '(("^\\*Completions" :ignore t)
+    ("^\\*Local variables\\*$"
+     :vslot -1 :slot 1 :size +popup-shrink-to-fit)
+    ("^\\*\\(?:[Cc]ompil\\(?:ation\\|e-Log\\)\\|Messages\\)"
+     :vslot -2 :size 0.3  :autosave t :quit t :ttl nil)
+    ("^\\*\\(?:doom \\|Pp E\\)"  ; transient buffers (no interaction required)
+     :vslot -3 :size +popup-shrink-to-fit :autosave t :select ignore :quit t :ttl 0)
+    ("^\\*\\(?:v?term\\|e?shell\\)"  ; editing buffers (interaction required)
+     :vslot -5 :size 0.25 :select t :modeline nil :quit nil :ttl nil)
+    ("^\\*\\(?:Wo\\)?Man "
+     :vslot -6 :size 0.45 :select t :quit t :ttl 0)
+    ("^\\*Calc"
+     :vslot -7 :side bottom :size 0.4 :select t :quit nil :ttl 0)
+    ("^\\*Customize"
+     :slot 2 :side right :size 0.5 :select t :quit nil)
+    ("^ \\*undo-tree\\*"
+     :slot 2 :side left :size 20 :select t :quit t)
+    ;; `help-mode', `helpful-mode'
+    ("^\\*\\([Hh]elp\\|Apropos\\)"
+     :slot 2 :vslot -8 :size 0.42 :select t)
+    ("^\\*eww\\*"  ; `eww' (and used by dash docsets)
+     :vslot -11 :size 0.35 :select t)
+    ("^\\*xwidget"
+     :vslot -11 :size 0.35 :select nil)
+    ("^\\*info\\*$"  ; `Info-mode'
+     :slot 2 :vslot 2 :size 0.45 :select t)
+    ("^\\*docker" :vslot -5 :size 0.5 :select t :modeline nil :quit nil :ttl nil)
+    ("^\\*Warnings" :vslot 99 :size 0.25)
+    ("^\\*Backtrace" :vslot 99 :size 0.4 :quit nil)
+    ("^\\*CPU-Profiler-Report "    :side bottom :vslot 100 :slot 1 :height 0.4 :width 0.5 :quit nil)
+    ("^\\*Memory-Profiler-Report " :side bottom :vslot 100 :slot 2 :height 0.4 :width 0.5 :quit nil)
+    ("^\\*Process List\\*" :side bottom :vslot 101 :size 0.25 :select t :quit t)
+    ("^\\*\\(?:Proced\\|timer-list\\|Abbrevs\\|Output\\|Occur\\|unsent mail.*?\\|message\\)\\*" :ignore t)))
 
 (add-hook 'after-init-hook #'+popup-mode 'append)
 
